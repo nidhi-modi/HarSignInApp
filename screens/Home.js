90dfc599d38649d080d439ee364de46d27dbd3d6
Nidhi Modi <modini@tandg.global>
2021-03-26 10:43:55 +1300

    view

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet, Text, View, TouchableOpacity, Image, FlatList, Dimensions, ImageBackground
+  StyleSheet, Text, View, Image, TouchableOpacity,
+  FlatList,
+  Dimensions,
+  Alert, ImageBackground
 } from 'react-native';
+
+import { ScrollView } from 'react-native-gesture-handler';
+
 
 var { width } = Dimensions.get('window');
 
@@ -12,29 +18,56 @@ export default class Home extends Component {
     super(props);
     this.state = {
 
+      modalVisible: false,
+      userSelected: [],
+      data: [
+        { id: 1, name: "Sign In", image: require("../assets/signInCard.png") },
+        { id: 2, name: "Site Map", image: "https://img.icons8.com/color/100/000000/real-estate.png" },
 
-    }
+      ]
+    };
+  }
+
+  clickEventListener = (item) => {
+    Alert.alert('Message', 'Item clicked. ' + item.name);
   }
 
   render() {
     return (
 
-      <View style={styles.container}>
+      <ScrollView keyboardShouldPersistTaps='handled'>
 
-        
-        <ImageBackground
-          style={{ width: width, height: 500, position: 'relative', top: 2, right: 2}}
+        <Image
+          style={{ width: width, height: 500 }}
           resizeMode={'cover'}
           source={require('../assets/tandgImage.jpg')}
-        >
+        />
 
-          <Text
-          style={{fontWeight: 'bold', color: 'black', position: 'absolute', top: 0, right: 0, padding:15}}
-          >
-            Welcome To HAR S
-          </Text>
-          </ImageBackground>
-      </View>
+        <View style={styles.container}>
+          <FlatList
+            style={styles.contentList}
+            columnWrapperStyle={styles.listContainer}
+            data={this.state.data}
+            keyExtractor={(item) => {
+              return item.id;
+            }}
+            renderItem={({ item }) => {
+              return (
+                <TouchableOpacity style={styles.card} onPress={() => { this.clickEventListener(item) }}>
+                  <Image style={styles.image} source={{ uri: item.image }} />
+                  <View style={styles.cardContent}>
+                    <Text style={styles.name}>{item.name}</Text>
+                    <Text style={styles.count}>{item.count}</Text>
+                    <TouchableOpacity style={styles.followButton} onPress={() => this.clickEventListener(item)}>
+                      <Text style={styles.followButtonText}>Explore now</Text>
+                    </TouchableOpacity>
+                  </View>
+                </TouchableOpacity>
+              )
+            }} />
+        </View>
+
+      </ScrollView>
 
 
     );
@@ -47,15 +80,75 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 
-  backgroundImage: {
-
-    width: width * 0.5,
-    height: 100,
-
+  container: {
+    flex: 1,
+    marginTop: 20,
+    backgroundColor: "#ebf0f7"
+  },
+  contentList: {
+    flex: 1,
+  },
+  cardContent: {
+    marginLeft: 20,
+    marginTop: 10
+  },
+  image: {
+    width: 90,
+    height: 90,
+    borderRadius: 45,
+    borderWidth: 2,
+    borderColor: "#ebf0f7"
   },
 
-  thumbnail: {
+  card: {
+    shadowColor: '#00000021',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+    elevation: 12,
 
-    resizeMode: 'cover'
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+    backgroundColor: "white",
+    padding: 10,
+    flexDirection: 'row',
+    borderRadius: 30,
   },
+
+  name: {
+    fontSize: 18,
+    flex: 1,
+    alignSelf: 'center',
+    color: "#3399ff",
+    fontWeight: 'bold'
+  },
+  count: {
+    fontSize: 14,
+    flex: 1,
+    alignSelf: 'center',
+    color: "#6666ff"
+  },
+  followButton: {
+    marginTop: 10,
+    height: 35,
+    width: 100,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "#dcdcdc",
+  },
+  followButtonText: {
+    color: "#dcdcdc",
+    fontSize: 12,
+  },
+
+
 });  
